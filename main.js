d1911c086f2d7ca7a3357c8ade9c1db59889b45d
Luan-Pinheiro <luan.p12@homtail.com>
2022-12-14 20:50:08 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function carregar(){
+    let text = document.getElementById('msg')
+    let img = document.getElementById('imagem');
+    let data = new Date();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    if(hora>1 && minuto > 1){
+        text.innerText = `Agora são ${hora} horas e ${minuto} minutos`
+    }else if( hora == 1 && minuto != 1){
+        text.innerText = `Agora são ${hora} hora e ${minuto} minutos`
+    }else if( hora != 1 && minuto == 1){
+        text.innerText = `Agora são ${hora} horas e ${minuto} minuto`
+    }else {
+        text.innerText = `Agora são ${hora} hora e ${minuto} minuto`
+    }
+    if(hora >= 0 && hora < 12){
+        img.src = 'manha1.png';
+        document.body.style.background = '#faea5c'
+    }else 
+        if(hora >= 12 && hora < 18){
+            img.src = 'tarde1.png';
+            document.body.style.background = 'bisque'
+        }
+        else{
+            img.src = 'noite1.png'
+            document.body.style.background = '#1a5353'
+        }
+}
